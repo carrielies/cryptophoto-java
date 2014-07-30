d7887c60657332a77efe0c020b4b67e186956258
Octavian Nita <mnothro@yahoo.com>
2014-07-30 20:40:51 +0200

    more flexible test bed (accepts cmd-line params)

--- src/main/java/com/cryptophoto/CPTest.java
+++ src/main/java/com/cryptophoto/CPTest.java
@@ -4,68 +4,74 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.Authenticator;
 import java.net.HttpURLConnection;
-import java.net.PasswordAuthentication;
 import java.net.URL;
 
 import static com.cryptophoto.CryptoPhotoUtils.CryptoPhotoResponse;
+import static java.lang.System.getProperty;
 import static java.lang.System.out;
 
 public class CPTest {
 
+    static String publicKey;
+
+    static String privateKey;
+
+    static String userId;
+
+    static String ip;
+
+    static String selector;
+
+    static String row;
+
+    static String col;
+
+    static String cph;
+
     static {
-        System.setProperty("http.proxyHost", "158.169.131.13");
-        System.setProperty("http.proxyPort", "8012");
-        System.setProperty("http.proxyUser", "nitanoc");
-        System.setProperty("http.proxyPassword", "th81on$");
-        System.setProperty("https.proxyHost", "158.169.131.13");
-        System.setProperty("https.proxyPort", "8012");
-        System.setProperty("https.proxyUser", "nitanoc");
-        System.setProperty("https.proxyPassword", "th81on$");
+        // Try to read test parameters as Java system properties given from the command line (e.g. -DpublicKey=...)
+        // or use defaults!
 
-        Authenticator.setDefault(new Authenticator() {
+        publicKey = getProperty("publicKey", "efe925bda3bc2b5cd6fe3ad3661075a7");
+        privateKey = getProperty("privateKey", "384b1bda2dafcd909f607083da22fef0");
 
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("nitanoc", "th81on$".toCharArray());
-            }
-        });
-    }
+        userId = getProperty("userId", "octavian");
 
-    static String visibleIp() {
-        String ip = null;
-
-        InputStream in = null;
-        try {
-            HttpURLConnection connection = (HttpURLConnection) new URL("https://cp.vu/show_my_ip").openConnection();
-            ip = new BufferedReader(new InputStreamReader(in = connection.getInputStream())).readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        ip = getProperty("ip");
+        if (ip == null || ip.trim().length() == 0) {
+            InputStream in = null;
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL("https://cp.vu/show_my_ip").openConnection();
+                ip = new BufferedReader(new InputStreamReader(in = connection.getInputStream())).readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
 
-        return ip;
+        selector = getProperty("selector", "selector");
+        row = getProperty("row", "row");
+        col = getProperty("col", "col");
+        cph = getProperty("cph", "cph");
     }
 
     public static void main(String[] args) throws Exception {
         // CryptoPhoto 'client':
-        CryptoPhotoUtils cryptoPhoto =
-            new CryptoPhotoUtils("efe925bda3bc2b5cd6fe3ad3661075a7", "384b1bda2dafcd909f607083da22fef0");
+        CryptoPhotoUtils cryptoPhoto = new CryptoPhotoUtils(publicKey, privateKey);
 
         // What's my visible IP?
-        String ip = visibleIp();
         out.printf("Visible IP: %s%n%n", ip);
 
         // Establish a CryptoPhoto session:
-        CryptoPhotoResponse cryptoPhotoSession = cryptoPhoto.getSession("octavian", ip);
+        CryptoPhotoResponse cryptoPhotoSession = cryptoPhoto.getSession(userId, ip);
 
         if (!cryptoPhotoSession.is("valid")) {
             out.printf("CryptoPhoto session not established!%nERROR: %s, %s%n%n", cryptoPhotoSession.get("error"),
@@ -83,8 +89,7 @@ public class CPTest {
 
         // Ask CryptoPhoto to verify a user response:
         // (! CODE SAMPLE ONLY! REQUIRES ACTUAL CORRECT USER INPUT TO SUCCEED !)
-        CryptoPhotoResponse cryptoPhotoVerification =
-            cryptoPhoto.verify("selector", "row", "col", "cph", "octavian", ip);
+        CryptoPhotoResponse cryptoPhotoVerification = cryptoPhoto.verify("selector", "row", "col", "cph", userId, ip);
 
         boolean success = cryptoPhotoVerification.is("valid");
         out.printf("%nCryptoPhoto verification %s: %s%n%n", success ? "succeeded" : "failed",
